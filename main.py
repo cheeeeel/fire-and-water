ec85753e18cf769b38ec03db935c7d35be12a58b
krissss <kriszz13@mail.ru>
2021-12-16 16:24:34 +0300

    test commit

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ class MainMenu:
         while pygame.event.wait().type != pygame.QUIT:
             pygame.display.flip()
 
+    def test(self):
+        print("Hello vlad")
 
 if __name__ == '__main__':
     MainMenu()
