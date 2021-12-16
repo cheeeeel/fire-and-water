d46824e14939bf053fa3a8c467f61dbad05e2511
Andrew <alekseev2005amg@gmail.com>
2021-12-16 14:19:06 +0300

    16.12.2021

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+class MainMenu:
+    def __init__(self):
+        pygame.init()
+        size = width, height = 600, 600
+        self.screen = pygame.display.set_mode(size)
+        pygame.display.set_caption('God_of_natural')
+
+        while pygame.event.wait().type != pygame.QUIT:
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    MainMenu()
