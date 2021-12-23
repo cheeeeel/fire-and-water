6a0e61a67b3e36fa8123fc95c0960659a875dfe6
Andrew <alekseev2005amg@gmail.com>
2021-12-23 20:03:14 +0300

    final menu

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import os
 class MainMenu:
     def __init__(self, screen, width, height):
         self.start_screen(width, height)
-        self.main_font = pygame.font.SysFont('Comic Sans MS', round(height // 5 * 0.3))
+        self.main_font = pygame.font.SysFont('Segoe Print', round(height // 5 * 0.3))
 
-        setting_image = pygame.transform.scale(self.load_image("settings.png", -1), (width // 10, height // 8))
-        screen.blit(setting_image, (width * 0.89, 15))
+        self.setting_image = pygame.transform.scale(
+            self.load_image("settings.png", -1), (width // 10, height // 8))
+        screen.blit(self.setting_image, (width * 0.89, height // 75))
 
         self.txt_online_btn = self.main_font.render('Игра по сети', True, (200, 200, 200))
         screen.blit(self.txt_online_btn, (width // 75, height * 0.83))
@@ -20,8 +21,12 @@ class MainMenu:
         screen.blit(self.txt_redactor, (width // 75, height * 0.76))
 
         font_for_title = pygame.font.SysFont('Comic Sans MS', round(height // 5 * 0.6))
-        title = font_for_title.render('?name of game?', True, (255, 150, 0))
-        screen.blit(title, ((width - title.get_width()) // 2, round(height // 2.7)))
+        fire = font_for_title.render('Огонь', True, (255, 100, 0))
+        screen.blit(fire, ((width - fire.get_width() * 2.15) // 2, round(height // 2.7)))
+        _and_ = font_for_title.render('и', True, (255, 255, 255))
+        screen.blit(_and_, (width // 2, round(height // 2.7)))
+        water = font_for_title.render('Вода', True, (0, 0, 255))
+        screen.blit(water, ((width + water.get_width() * 0.7) // 2, round(height // 2.7)))
 
     def load_image(self, s, key=None):
         name = os.path.join("data", s)
@@ -39,14 +44,14 @@ class MainMenu:
         return image
 
     def go_next(self, x, y, width, height):
-        len_1 = self.txt_online_btn.get_width()
-        len_2 = self.txt_one_pc_btn.get_width()
-        len_3 = self.txt_redactor.get_width()
-        if width // 75 <= x <= width // 75 + len_2 and height * 0.91 <= y <= height * 0.96:
+        if width // 75 <= x <= width // 75 + self.txt_one_pc_btn.get_width() \
+                and height * 0.91 <= y <= height * 0.96:
             print("""Один компьютер""")
-        elif width // 75 <= x <= width // 75 + len_3 and height * 0.77 <= y <= height * 0.82:
+        elif width // 75 <= x <= width // 75 + self.txt_redactor.get_width() \
+                and height * 0.77 <= y <= height * 0.82:
             print("""Создать карту""")
-        elif width // 75 <= x <= width // 75 + len_1 and height * 0.84 <= y <= height * 0.89:
+        elif width // 75 <= x <= width // 75 + self.txt_online_btn.get_width() \
+                and height * 0.84 <= y <= height * 0.89:
             print("""Игра по сети""")
 
     def set_color(self, x, y, width, height):
@@ -59,13 +64,20 @@ class MainMenu:
         elif width // 75 <= x <= width // 75 + self.txt_online_btn.get_width() \
                 and height * 0.84 <= y <= height * 0.89:
             self.txt_online_btn = self.main_font.render('Игра по сети', True, (255, 201, 0))
+        elif width * 0.89 <= x <= width * 0.89 + width // 10 \
+                and height // 75 <= y <= height // 75 + height // 8:
+            self.setting_image = pygame.transform.scale(
+                self.load_image("settings_mouse.png", -1), (width // 10, height // 8))
         else:
             self.txt_online_btn = self.main_font.render('Игра по сети', True, (200, 200, 200))
             self.txt_one_pc_btn = self.main_font.render('Один компьютер', True, (200, 200, 200))
             self.txt_redactor = self.main_font.render('Создать карту', True, (200, 200, 200))
+            self.setting_image = pygame.transform.scale(
+                self.load_image("settings.png", -1), (width // 10, height // 8))
         screen.blit(self.txt_online_btn, (width // 75, height * 0.83))
         screen.blit(self.txt_one_pc_btn, (width // 75, height * 0.9))
         screen.blit(self.txt_redactor, (width // 75, height * 0.76))
+        screen.blit(self.setting_image, (width * 0.89, height // 75))
 
     def start_screen(self, width, height):
         fon = pygame.transform.scale(self.load_image('main_menu_picture.jpg'), (width, height))
