60acced20e353c9aa0d75f34cee9674ddc803007
Andrew <alekseev2005amg@gmail.com>
2021-12-23 14:40:36 +0300

    lights for buttons on main menu

--- main.py
+++ main.py
@@ -5,22 +5,23 @@ import os
 class MainMenu:
     def __init__(self, screen, width, height):
         self.start_screen(width, height)
-        main_font = pygame.font.SysFont('Comic Sans MS', round(height // 5 * 0.5))
+        self.main_font = pygame.font.SysFont('Comic Sans MS', round(height // 5 * 0.3))
 
-        setting_image = self.load_image("settings.png", -1)
-        screen.blit(setting_image, (width - 115, 15))
+        setting_image = pygame.transform.scale(self.load_image("settings.png", -1), (width // 10, height // 8))
+        screen.blit(setting_image, (width * 0.89, 15))
 
-        self.txt_settings_btn = main_font.render('Игра по сети', True, (255, 201, 0))
-        screen.blit(self.txt_settings_btn,
-                    ((width - self.txt_settings_btn.get_width()) // 2, round(height // 5 * 2.75)))
+        self.txt_online_btn = self.main_font.render('Игра по сети', True, (200, 200, 200))
+        screen.blit(self.txt_online_btn, (width // 75, height * 0.83))
 
-        self.txt_duo_btn = main_font.render('Один компьютер', True, (255, 201, 0))
-        screen.blit(self.txt_duo_btn,
-                    ((width - self.txt_duo_btn.get_width()) // 2, round(height // 5 * 3.5)))
+        self.txt_one_pc_btn = self.main_font.render('Один компьютер', True, (200, 200, 200))
+        screen.blit(self.txt_one_pc_btn, (width // 75, height * 0.9))
+
+        self.txt_redactor = self.main_font.render('Создать карту', True, (200, 200, 200))
+        screen.blit(self.txt_redactor, (width // 75, height * 0.76))
 
         font_for_title = pygame.font.SysFont('Comic Sans MS', round(height // 5 * 0.6))
-        title = font_for_title.render('?name of game?', True, (255, 201, 0))
-        screen.blit(title, ((width - title.get_width()) // 2, round(height // 5 * 1.1)))
+        title = font_for_title.render('?name of game?', True, (255, 150, 0))
+        screen.blit(title, ((width - title.get_width()) // 2, round(height // 2.7)))
 
     def load_image(self, s, key=None):
         name = os.path.join("data", s)
@@ -38,14 +39,33 @@ class MainMenu:
         return image
 
     def go_next(self, x, y, width, height):
-        m = self.txt_settings_btn.get_width()
-        n = self.txt_duo_btn.get_width()
-        if (width - m) // 2 <= x <= (width + m) // 2 and height // 5 * 2.8 <= y <= round(height * 3.35 // 5):
-            print("""""")
-        elif (width - n) // 2 <= x <= (width + n) // 2 and height // 5 * 3.65 <= y <= round(height * 4.1 // 5):
-            print("""""")
-        elif width - 115 <= x <= width - 15 and 15 <= y <= 115:
-            print("""""")
+        len_1 = self.txt_online_btn.get_width()
+        len_2 = self.txt_one_pc_btn.get_width()
+        len_3 = self.txt_redactor.get_width()
+        if width // 75 <= x <= width // 75 + len_2 and height * 0.91 <= y <= height * 0.96:
+            print("""Один компьютер""")
+        elif width // 75 <= x <= width // 75 + len_3 and height * 0.77 <= y <= height * 0.82:
+            print("""Создать карту""")
+        elif width // 75 <= x <= width // 75 + len_1 and height * 0.84 <= y <= height * 0.89:
+            print("""Игра по сети""")
+
+    def set_color(self, x, y, width, height):
+        if width // 75 <= x <= width // 75 + self.txt_one_pc_btn.get_width() \
+                and height * 0.91 <= y <= height * 0.96:
+            self.txt_one_pc_btn = self.main_font.render('Один компьютер', True, (255, 201, 0))
+        elif width // 75 <= x <= width // 75 + self.txt_redactor.get_width() \
+                and height * 0.77 <= y <= height * 0.82:
+            self.txt_redactor = self.main_font.render('Создать карту', True, (255, 201, 0))
+        elif width // 75 <= x <= width // 75 + self.txt_online_btn.get_width() \
+                and height * 0.84 <= y <= height * 0.89:
+            self.txt_online_btn = self.main_font.render('Игра по сети', True, (255, 201, 0))
+        else:
+            self.txt_online_btn = self.main_font.render('Игра по сети', True, (200, 200, 200))
+            self.txt_one_pc_btn = self.main_font.render('Один компьютер', True, (200, 200, 200))
+            self.txt_redactor = self.main_font.render('Создать карту', True, (200, 200, 200))
+        screen.blit(self.txt_online_btn, (width // 75, height * 0.83))
+        screen.blit(self.txt_one_pc_btn, (width // 75, height * 0.9))
+        screen.blit(self.txt_redactor, (width // 75, height * 0.76))
 
     def start_screen(self, width, height):
         fon = pygame.transform.scale(self.load_image('main_menu_picture.jpg'), (width, height))
@@ -70,6 +90,8 @@ if __name__ == '__main__':
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 main.go_next(*event.pos, *size)
+            if event.type == pygame.MOUSEMOTION:
+                main.set_color(*event.pos, *size)
         pygame.display.flip()
 
     pygame.quit()
