df8652ebf0d2a77346fa149aeb894ecde7f011f5
Andrew <alekseev2005amg@gmail.com>
2021-12-24 16:33:52 +0300

    new menu fon, generally more beautiful

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ import os
 
 
 class MainMenu:
-    def __init__(self, screen, width, height):
+    def __init__(self, width, height):
+        self.make_inscriptions(width, height)
+
+    def make_inscriptions(self, width, height, col_redactor='white',
+                          col_single='white', col_online='white', flag_settings=False):
         self.start_screen(width, height)
         self.main_font = pygame.font.SysFont('Segoe Print', round(height // 5 * 0.3))
 
-        self.setting_image = pygame.transform.scale(
-            self.load_image("settings.png", -1), (width // 10, height // 8))
+        file = self.load_image("settings_mouse.png", -1) \
+            if flag_settings else self.load_image("settings.png", -1)
+        self.setting_image = pygame.transform.scale(file, (width // 10, height // 8))
         screen.blit(self.setting_image, (width * 0.89, height // 75))
 
-        self.txt_online_btn = self.main_font.render('Игра по сети', True, (200, 200, 200))
+        self.txt_online_btn = self.main_font.render('Игра по сети', True, col_online)
         screen.blit(self.txt_online_btn, (width // 75, height * 0.83))
 
-        self.txt_one_pc_btn = self.main_font.render('Один компьютер', True, (200, 200, 200))
+        self.txt_one_pc_btn = self.main_font.render('Один компьютер', True, col_single)
         screen.blit(self.txt_one_pc_btn, (width // 75, height * 0.9))
 
-        self.txt_redactor = self.main_font.render('Создать карту', True, (200, 200, 200))
+        self.txt_redactor = self.main_font.render('Создать карту', True, col_redactor)
         screen.blit(self.txt_redactor, (width // 75, height * 0.76))
 
+        # all_title = fire + and + water
         font_for_title = pygame.font.SysFont('Comic Sans MS', round(height // 5 * 0.6))
         fire = font_for_title.render('Огонь', True, (255, 100, 0))
         screen.blit(fire, ((width - fire.get_width() * 2.15) // 2, round(height // 2.7)))
@@ -45,39 +51,33 @@ class MainMenu:
 
     def go_next(self, x, y, width, height):
         if width // 75 <= x <= width // 75 + self.txt_one_pc_btn.get_width() \
-                and height * 0.91 <= y <= height * 0.96:
+                and height * 0.92 <= y <= height:
             print("""Один компьютер""")
         elif width // 75 <= x <= width // 75 + self.txt_redactor.get_width() \
-                and height * 0.77 <= y <= height * 0.82:
+                and height * 0.78 <= y <= height * 0.84:
             print("""Создать карту""")
         elif width // 75 <= x <= width // 75 + self.txt_online_btn.get_width() \
-                and height * 0.84 <= y <= height * 0.89:
+                and height * 0.85 <= y <= height * 0.91:
             print("""Игра по сети""")
+        elif width * 0.89 <= x <= width * 0.89 + width // 10 \
+                and height // 75 <= y <= height // 75 + height // 8:
+            print("""Настройки""")
 
     def set_color(self, x, y, width, height):
         if width // 75 <= x <= width // 75 + self.txt_one_pc_btn.get_width() \
-                and height * 0.91 <= y <= height * 0.96:
-            self.txt_one_pc_btn = self.main_font.render('Один компьютер', True, (255, 201, 0))
+                and height * 0.92 <= y <= height * 0.99:
+            self.make_inscriptions(width, height, col_single='yellow')
         elif width // 75 <= x <= width // 75 + self.txt_redactor.get_width() \
-                and height * 0.77 <= y <= height * 0.82:
-            self.txt_redactor = self.main_font.render('Создать карту', True, (255, 201, 0))
+                and height * 0.78 <= y <= height * 0.83:
+            self.make_inscriptions(width, height, col_redactor='yellow')
         elif width // 75 <= x <= width // 75 + self.txt_online_btn.get_width() \
-                and height * 0.84 <= y <= height * 0.89:
-            self.txt_online_btn = self.main_font.render('Игра по сети', True, (255, 201, 0))
+                and height * 0.85 <= y <= height * 0.9:
+            self.make_inscriptions(width, height, col_online='yellow')
         elif width * 0.89 <= x <= width * 0.89 + width // 10 \
                 and height // 75 <= y <= height // 75 + height // 8:
-            self.setting_image = pygame.transform.scale(
-                self.load_image("settings_mouse.png", -1), (width // 10, height // 8))
+            self.make_inscriptions(width, height, flag_settings=True)
         else:
-            self.txt_online_btn = self.main_font.render('Игра по сети', True, (200, 200, 200))
-            self.txt_one_pc_btn = self.main_font.render('Один компьютер', True, (200, 200, 200))
-            self.txt_redactor = self.main_font.render('Создать карту', True, (200, 200, 200))
-            self.setting_image = pygame.transform.scale(
-                self.load_image("settings.png", -1), (width // 10, height // 8))
-        screen.blit(self.txt_online_btn, (width // 75, height * 0.83))
-        screen.blit(self.txt_one_pc_btn, (width // 75, height * 0.9))
-        screen.blit(self.txt_redactor, (width // 75, height * 0.76))
-        screen.blit(self.setting_image, (width * 0.89, height // 75))
+            self.make_inscriptions(width, height)
 
     def start_screen(self, width, height):
         fon = pygame.transform.scale(self.load_image('main_menu_picture.jpg'), (width, height))
@@ -93,7 +93,7 @@ if __name__ == '__main__':
     size = 1000, 800
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('God_of_natural')
-    main = MainMenu(screen, *size)
+    main = MainMenu(*size)
     running = True
 
     while running:
