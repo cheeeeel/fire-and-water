5411628d8fc063cf52888f8cf5cac02b8b043253
Andrew <alekseev2005amg@gmail.com>
2021-12-27 19:33:47 +0300

    new objects (activate button and barrier) that we might locate in the map

--- creating_levels.py
+++ creating_levels.py
@@ -1,6 +1,10 @@
 import pygame
 import os
-from time import sleep
+
+
+barriers = {}
+buttons = {}
+
 
 def load_image(s, key=None):
     name = os.path.join("data", s)
@@ -33,10 +37,22 @@ class Level:
         self.left = 20
         self.top = 28
         self.cell_size = 24
-        self.photo = load_image("stone.png", -1)
+        self.object = ["stone.png", "barrier.png", "activate_button.png"]
+        self.obj_index = 0
         self.floor()
         self.default_color()
+        self.helper_bar, self.helper_btn = 0, 0
+
         self.main_font = pygame.font.SysFont('Segoe Print', 25)
+
+    # настройка внешнего вида
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def set_object(self, index):
+        return load_image(self.object[index])
 
     def default_color(self):
         self.clear_map_color = "white"
@@ -56,11 +72,18 @@ class Level:
 
     # отрисовывает карту
     def render(self, screen):
-        self.current_object = pygame.transform.scale(self.photo, (24, 24))
+        self.current_object = pygame.transform.scale(self.set_object(self.obj_index), (24, 24))
         for y in range(self.height):
             for x in range(self.width):
-                if self.board[x][y]:
-                    screen.blit(self.current_object, (x * 24 + 20, y * 24 + 28))
+                if self.board[x][y] == 1:
+                    stone = pygame.transform.scale(load_image("stone.png"), (24, 24))
+                    screen.blit(stone, (x * 24 + 20, y * 24 + 28))
+                elif self.board[x][y] == 2:
+                    bar = pygame.transform.scale(load_image("barrier.png"), (24, 24))
+                    screen.blit(bar, (x * 24 + 20, y * 24 + 28))
+                elif self.board[x][y] == 3:
+                    btn = pygame.transform.scale(load_image("activate_button.png"), (48, 24))
+                    screen.blit(btn, (x * 24 + 20, y * 24 + 28))
 
         for y in range(self.height):
             for x in range(self.width):
@@ -87,21 +110,21 @@ class Level:
                 return cell_coords
 
     # рассматривает координаты
-    def get_click(self, mouse_pos):
+    def get_click(self, mouse_pos, key_for_bar=None):
         if 780 < mouse_pos[1] < 780 + self.clear_map.get_height():
             if 390 < mouse_pos[0] < 390 + self.clear_map.get_width():
                 self.clear()
             elif 30 < mouse_pos[0] < 30 + self.change_object.get_width():
-                print("Сменить объект")
+                self.obj_index = (self.obj_index + 1) % len(self.object)
             elif 730 < mouse_pos[0] < 730 + self.save_map.get_width():
                 self.save()
 
         cell = self.get_cell(mouse_pos)
         if cell is None:
             return
-        self.on_click(cell)
+        self.on_click(cell, key_for_bar)
 
-    # очисчает поле
+    # очищает поле
     def clear(self):
         for y in range(self.height):
             for x in range(self.width):
@@ -122,10 +145,53 @@ class Level:
                 f.write(row + "\n")
 
     # обновляет значение ячейки на поле
-    def on_click(self, cell_coords):
+    def on_click(self, cell_coords, key_for_bar=None):
         i = cell_coords[0]
         j = cell_coords[1]
-        self.board[i][j] = 1 - self.board[i][j]
+        if self.board[i][j] != 2 and self.board[i][j] != 0:
+            self.board[i][j] = 0
+            return
+        elif self.board[i][j] == 2:
+            self.delete_barrier(cell_coords)
+            return
+        elif self.board[i - 1][j] == 3:
+            self.board[i - 1][j] = 0
+            return
+        if self.obj_index == 1:
+            self.helper_bar += 1
+            if key_for_bar:
+                if self.width - i <= 5:
+                    barriers[self.helper_bar] = []
+                    for num in range(1, 6):
+                        self.board[self.width - num][j] = 2
+                        barriers[self.helper_bar].append((self.width - num, j))
+                else:
+                    barriers[self.helper_bar] = []
+                    for num in range(5):
+                        self.board[i + num][j] = 2
+                        barriers[self.helper_bar].append((i + num, j))
+            elif not key_for_bar:
+                if self.height - j >= self.height - 5:
+                    barriers[self.helper_bar] = []
+                    for num in range(5):
+                        self.board[i][num] = 2
+                        barriers[self.helper_bar].append((i, num))
+                else:
+                    barriers[self.helper_bar] = []
+                    for num in range(5):
+                        self.board[i][j - num] = 2
+                        barriers[self.helper_bar].append((i, j - num))
+        elif self.obj_index == 2:
+            buttons[self.helper_btn] = []
+            if i + 1 == self.width:
+                self.board[i - 1][j] = 3
+                buttons[self.helper_btn].extend([(i - 1, j), (i, j)])
+            else:
+                self.board[i][j] = 3
+                buttons[self.helper_btn].extend([(i, j), (i + 1, j)])
+            self.helper_btn += 1
+        else:
+            self.board[i][j] = self.obj_index + 1 - self.board[i][j]
         # определяет объект по размеру(криво)
         # if "622" in str(self.photo):
         #     self.board[i][j] = "water"
@@ -146,31 +212,34 @@ class Level:
         else:
             self.default_color()
 
+    # обработка удаления барьера
+    def delete_barrier(self, coords):
+        for i in list(barriers.keys()):
+            if any(coords == k for k in barriers[i]):
+                for x, y in barriers[i]:
+                    self.board[x][y] = 0
+                barriers.pop(i)
+
 
 pygame.init()
 size = 1000, 840
 screen = pygame.display.set_mode(size)
-place_delay = pygame.USEREVENT + 1
 level = Level(40, 31)
-left_button_pressed = False
 running = True
-pos = (0, 0)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            left_button_pressed = True
-            pygame.time.set_timer(place_delay, 200)
-        if event.type == pygame.MOUSEBUTTONUP:
-            left_button_pressed = False
-            pygame.time.set_timer(place_delay, 0)
+            if event.button == 1:
+                level.get_click(event.pos, True)
+            elif event.button == 3:
+                level.get_click(event.pos, False)
+            else:
+                level.get_click(event.pos)
         if event.type == pygame.MOUSEMOTION:
             level.set_color(event.pos)
-            pos = event.pos
-        if event.type == place_delay and left_button_pressed:
-            level.get_click(pos)
-    screen.fill("black")
-    level.render(screen)
-    pygame.display.flip()
+        screen.fill("black")
+        level.render(screen)
+        pygame.display.flip()
 pygame.quit()
